1d4717772246c2f6fc79525ce2e103af89c93175
17259235 <17259235@DSN1271058245.cfp127aluno.local>
2018-04-05 10:31:52 -0300

    Get e Set adicionados

--- ProjetoAgenda/src/br/senai/sp/jandira/model/Contato.java
+++ ProjetoAgenda/src/br/senai/sp/jandira/model/Contato.java
@@ -5,9 +5,66 @@ public class Contato {
 	private int id;
 	private String nome;
 	private String telefone;
-	private String  celular;
+	private String celular;
 	private String email;
 	private String dtNascimento;
 	private String endereço;
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCelular() {
+		return celular;
+	}
+
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDtNascimento() {
+		return dtNascimento;
+	}
+
+	public void setDtNascimento(String dtNascimento) {
+		this.dtNascimento = dtNascimento;
+	}
+
+	public String getEndereço() {
+		return endereço;
+	}
+
+	public void setEndereço(String endereço) {
+		this.endereço = endereço;
+	}
+	
+	
 }
